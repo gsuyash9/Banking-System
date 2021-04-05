2f40aa43f82f7c57767060fa32b3a460436f3a45
gsuyash9 <2018256@iiitdmj.ac.in>
2021-04-06 01:58:53 +0530

    templates added

--- Bank/urls.py
+++ Bank/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.home, name='home'),
+    path('account',views.account, name='account'),
+    path('loans',views.loans, name='loans'),
+    path('cards',views.cards, name='cards'),
 ]
--- Bank/views.py
+++ Bank/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def account(request):
+    return render(request,'account.html')
+def loans(request):
+    return render(request,'loans.html')
+def cards(request):
+    return render(request,'cards.html')
